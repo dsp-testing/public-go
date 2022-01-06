6e96a2c8f73b84e975543a0a46eb5d58d835debe
David McIntosh <804610+mctofu@users.noreply.github.com>
2022-01-06 11:26:16 -0800

    Add a public dependency

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dsp-testing/public-go
+
+go 1.17
+
+require github.com/dsp-testing/can-be-publicized-go v1.0.0
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/dsp-testing/can-be-publicized-go"
+)
+
+func main () {
+	fmt.Println(canberemoved.Y)
+}
